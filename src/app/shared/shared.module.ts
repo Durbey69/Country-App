b35e9103ff4de83235a6c6845a0791e869c2b983
dchavarria <dchavarria@rentingcolombia.co>
2023-09-27 08:32:27 -0500

    rest countries

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+
+
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ContactPagesComponent } from './pages/contact-pages/contact-pages.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
+
+
+
+
+
+@NgModule({
+  declarations: [
+      AboutPageComponent,
+      HomePageComponent,
+      SidebarComponent,
+      ContactPagesComponent,
+      SearchBoxComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    AboutPageComponent,
+    HomePageComponent,
+    SidebarComponent,
+    ContactPagesComponent,
+    SearchBoxComponent
+
+  ]
+})
+export class SharedModule { }
