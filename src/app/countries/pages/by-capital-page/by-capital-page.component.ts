f07851e04245e16d8dbfc276959a5a51f3ab8dcc
dchavarria <dchavarria@rentingcolombia.co>
2023-09-27 09:57:57 -0500

    appcountry

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CountriesService } from '../../services/countries.service';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -8,8 +10,19 @@ import { Component } from '@angular/core';
 })
 export class ByCapitalPageComponent {
 
+
+  public countries: Country[] = []
+  constructor( private countriesServices: CountriesService){}
+
+
   searchByCapital(term:string):void{
-    console.log('Desde searchByCapitalPage')
+    this.countriesServices.searchByCapital(term)
+    .subscribe( countries =>{
+      this.countries = countries;
+    } )
+
+
+
     console.log({term})
 
   }
